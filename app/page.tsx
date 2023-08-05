ac2be918aee276edf27e25ab8437bfa9f77d3b17
michaelDonchenko <michael1994d@gmail.com>
2023-08-05 11:35:05 +0300

    reinstalled project

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@ import React from "react";
 type Props = {};
 
 const RootPage = (props: Props) => {
-  return <div>RootPage</div>;
+  return (
+    <div>
+      <h1>Data fetching in Nextjs</h1>
+    </div>
+  );
 };
 
 export default RootPage;
