fb11ad23c309ea41b580ad231f97e665215a3372
michaelDonchenko <michael1994d@gmail.com>
2023-08-05 14:21:51 +0300

    added max width

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} style={{maxWidth: "900px", margin: "0 auto"}}>
         <h1>Data fetching in Nextjs</h1>
         <Search />
         {children}
